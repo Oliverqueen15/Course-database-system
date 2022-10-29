d3342c905f8fed11cc41b7412d917aa10360b1a4
Arrowhead <victorfaith15@gmail.com>
2022-10-29 16:38:03 +0100

    first commit

---
+++ Lecturer_Login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Style.css">
+    <title>Lecturer Login Page</title>
+</head>
+
+<body>
+    <div class="container">
+       
+        <div class="page">
+            <div class="title"> Lecturer Portal
+                
+            </div>
+            <form action="">
+                <input type="text" placeholder="Name">
+                <input type="password" placeholder="password">
+                <button>ENTER</button>
+
+            </form>
+            <div>
+                <a href="#">Forget password?</a>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+            -
+            <div class="Register">
+                Don't have an account? <a href="#"> Register</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ Login.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Login.css">
+</head>
+
+<body>
+
+
+
+    <div class="right">
+        <div class="new">
+            WELCOME PAGE
+            <div>
+
+                <img src="pixel.jpg" alt="" srcset="">
+
+            </div>
+        </div>
+
+        <div class="right_form">
+            <form action="method=" post class='form'>
+
+                <h1>Register Lecturer</h1>
+                <div class="input_fields">
+                    <label for="name">Name</label><br>
+                    <input type="name" name="name" id="">
+                </div>
+                <div class="input_fields">
+                    <label for="date_of_birth">Date Of Birth</label><br>
+                    <input type="Date" name="date_of_birth" id="">
+                </div>
+                <div class="input_fields">
+                    <label for="phone">Phone</label><br>
+                    <input type="number" name="phone" id="">
+                </div>
+                <div class="input_fields">
+                    <label for="email">Email</label><br>
+                    <input type="email" name="email" id="">
+                </div>
+
+                <div class="input_fields">
+                    <label for="password">password</label><br>
+                    <input type="password" name="password" id="#">
+                </div>
+
+                <div class="btn">
+                    <input type="submit" name="btn_doctor" class="btn_doctor" id="btn_doctor">
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
